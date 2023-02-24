159c0216e9c7fa607937113705914c3650f9f9e0
Erison Mina <ecom.sysnextt@gmail.com>
2023-02-24 12:41:11 -0300

    Alteracao teste git

--- namer_app/flutter_application_1/lib/main.dart
+++ namer_app/flutter_application_1/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Meu App Teste',
+        title: 'Meu App Teste2',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
